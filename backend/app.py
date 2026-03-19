d7b4912eb50e6526b3714ac800823d5161275262
selvaganesh19 <selvavelayutham395@gmail.com>
2026-03-19 14:11:29 +0530

    changes in api keys

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 load_dotenv()
 
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
-OPENROUTER_MODEL = os.getenv("OPENROUTER_MODEL")
+OPENROUTER_MODEL = os.getenv("OPENROUTER_MODEL","z-ai/glm-4.5-air:free")
 
 app = Flask(__name__, static_folder='../frontend', static_url_path='')
 
